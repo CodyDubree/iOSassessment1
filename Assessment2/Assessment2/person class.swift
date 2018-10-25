6ccc133094fdfd76726e028d7345e36ace2999a8
Cody Dubree <codydubree@MacBook-Pro-24.local>
2018-10-25 12:34:02 -0500

    finished

---
+++ Assessment2/Assessment2/person class.swift
@@ -0,0 +1,23 @@
+//
+//  person class.swift
+//  Assessment2
+//
+//  Created by Cody Dubree on 10/25/18.
+//  Copyright © 2018 Cody Dubree. All rights reserved.
+//
+
+import Foundation
+
+
+
+
+class person {
+    var firstName: String
+    var lastName: String
+    var email: String
+    init(firstName : String, lastName : String, email : String) {
+        self.firstName = firstName
+        self.lastName = lastName
+        self.email = email
+    }
+}
